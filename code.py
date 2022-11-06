e9f9da857ceab4af6dbfc160de399bfa4220996b
Duarte Cruz <102768606+DuarteCruz6@users.noreply.github.com>
2022-11-06 15:27:36 +0000

    Create code.py

---
+++ code.py
@@ -0,0 +1,16 @@
+import random 
+pcNumber = random.randint(0,101)
+while True:
+    personNumber = int(input("chose a number: "))
+    if personNumber < pcNumber:
+        print("the number is greater than", personNumber)
+    elif  personNumber > pcNumber:
+        print("the number is lower than", personNumber)
+    else:
+        print("congrats, you got it right.")
+        playAgain = input("wanna play again? ")
+        if playAgain == "no":
+            print("okay, thanks for playing.")
+            break
+        else:
+            pcNumber = random.randint(0,101)
